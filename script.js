9a29cd7e8a6e04dd3c2c5faee747c423201d7eac
Rahma Berhan <122676773+RahmaB1@users.noreply.github.com>
2026-02-12 02:42:37 +0000

    .

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// This is a placeholder file which shows how you can access functions defined in other files.
-// It can be loaded into index.html.
-// You can delete the contents of the file once you have understood how it works.
-// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
-// You can't open the index.html file using a file:// URL.
-
 import { getUserIds, getData, setData, clearData} from "./storage.js";
 import { sortBookmarks } from "./logic.js";
 
@@ -65,6 +59,10 @@ let likes = 0;
   renderBookmarks(currentUserId);
 }
 );
+
+
+//----------------------  Bookmarks  ---------------------------
+
 
 // addBookmark() will add the new bookmark to the old ones then send them to saveBookmark()
 function addBookmark (bookmark){
